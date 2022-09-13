8f159bf9c1c27588a640f1313536868664d230d6
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-09-12 22:44:28 -0300

    web -> Next Step: Twitch API

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,4 @@
 git add .
-git commit -m "project inserted on GitHub"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/nlw-eSports.git
-git push -u origin main
+git commit -m "web -> Next Step: Twitch API"
+git push
 git status
