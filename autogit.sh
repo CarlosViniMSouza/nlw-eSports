774a4d458ad8b3b7730653a69f920a31464a5658
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-09-16 03:55:00 -0300

    mobile: concluded | Last commit this project! | Thanks for NLW eSports folks!!

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "apps web & server: concluded | Next step: mobile"
+git commit -m "mobile: concluded | Last commit this project! | Thanks for NLW eSports folks!!"
 git push
 git status
